894c48e2e887f4e0f3002297c990fd452c48567a
Gabriel Afonso <gabriel.afonso@newteq.de>
2022-12-06 19:41:44 +0100

    further oprimized day 6

--- day-6/main.go
+++ day-6/main.go
@@ -13,24 +13,18 @@ func main() {
 	}
 
 	for _, distinct := range [2]int{4, 14} {
-		for cursor := 0; cursor < len(dat)-distinct-1; {
-			next := checkSection(dat, cursor, distinct)
-			cursor += next
-			if next == 0 {
-				fmt.Println(cursor + distinct)
-				break
+	outer:
+		for c := 0; c < len(dat)-distinct-1; {
+			contains := make(map[byte]int, distinct)
+			for i := 0; i < distinct; i++ {
+				if contains[dat[c+i]] != 0 {
+					c += contains[dat[c+i]]
+					continue outer
+				}
+				contains[dat[c+i]] = i + 1
 			}
+			fmt.Println(c + distinct)
+			break
 		}
 	}
-}
-
-func checkSection(dat []byte, c int, sectionBits int) int {
-	contains := make(map[byte]int, sectionBits)
-	for i := 0; i < sectionBits; i++ {
-		if contains[dat[c+i]] != 0 {
-			return contains[dat[c+i]]
-		}
-		contains[dat[c+i]] = i + 1
-	}
-	return 0
 }
